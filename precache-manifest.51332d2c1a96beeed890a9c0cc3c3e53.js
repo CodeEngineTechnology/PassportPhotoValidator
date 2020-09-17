84e77cb4afceae50632a2de501b3fe34bdb570d1
CodeEngineTechnology <codeengine101@gmail.com>
2020-09-16 22:27:05 -0700

    Updates

---
+++ precache-manifest.51332d2c1a96beeed890a9c0cc3c3e53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb41da060aa19687a5d2b3f129023721",
+    "url": "/PassportPhotoValidator/index.html"
+  },
+  {
+    "revision": "1c2b257059eb6696fafc",
+    "url": "/PassportPhotoValidator/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "59fbead72227fb72fd16",
+    "url": "/PassportPhotoValidator/static/css/main.41a6f8dc.chunk.css"
+  },
+  {
+    "revision": "1c2b257059eb6696fafc",
+    "url": "/PassportPhotoValidator/static/js/2.c09b6a11.chunk.js"
+  },
+  {
+    "revision": "ac708def751a8292e55daf5717e57737",
+    "url": "/PassportPhotoValidator/static/js/2.c09b6a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59fbead72227fb72fd16",
+    "url": "/PassportPhotoValidator/static/js/main.d916e37b.chunk.js"
+  },
+  {
+    "revision": "680794bdc1a5dfec7d17",
+    "url": "/PassportPhotoValidator/static/js/runtime-main.f72a5459.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PassportPhotoValidator/static/media/logo.5d5d9eef.svg"
+  }
+]);
